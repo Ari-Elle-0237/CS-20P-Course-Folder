d701825ce4073ab9d32dcf8547047b6f9290fe80
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-09-05 13:23:22 -0700

    Updated Regex, added ui

--- Assigment 2/scramble.py
+++ Assigment 2/scramble.py
@@ -11,14 +11,16 @@ def main():
     print(f"Aríel Zepezauer, arielzepezauer@gmail.com\n"
           f"Due: Thu Sep 9, 2024 7:00pm, "
           f"Assignment #2: Program Two (Word Scrambler)")
+    ui = input()
+    print(scramble_word(ui))
 
-def scramble_string(string):
-    words = re.findall(r"\W?[A-Za-z]*\W?", string)
+def scramble_word(string):
+    words = re.findall(r"[\w']*", string)
     substitutions = []
     for word in words:
         first, last = word[0], word[-1]
         middle = word[1:-2]
-        middle = middle.scramble()
+        random.shuffle(middle)
         substitutions.append((first + middle + last, word))
     for substitution in substitutions:
         re.sub(substitution[0],substitution[1], string)
