115daa476b6ed2c08d34908b84015dcd5177a189
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-09-10 13:47:55 -0700

    updated docstring

--- Assignment_1/loan.py
+++ Assignment_1/loan.py
@@ -5,6 +5,7 @@ loan.py
 by Ariel Zepezauer (arielzepezauer@gmail.com
 Pengo: 'azepezau'
 Test Cases in loan_unittest.py
+Repository at: https://github.com/Ari-Elle-0237/CS-20P-Course-Folder.git
 """
 
 def main():
