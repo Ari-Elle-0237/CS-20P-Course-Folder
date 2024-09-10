8889a26ee5ce689c0b3e6f973819dacccaca1932
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-09-10 13:13:26 -0700

    Rearranged argunents, added more test cases

--- Assignment_1/loan.py
+++ Assignment_1/loan.py
@@ -3,6 +3,8 @@
 Assignment #1: Loan Payment Calculator
 loan.py
 by Ariel Zepezauer
+Exit Code: 0
+Test Cases in loan_unittest.py
 """
 
 def main():
@@ -10,12 +12,14 @@ def main():
           f"Due: Thu Sep 5, 2024 7:00pm, "
           f"Assignment #1: Program One (Loan Payment Calculator)")
     payment = calculate_payment(*prompt_user())
-    print(f"{payment:0.2f}")
+    print(f"each payment:${payment:0.2f}")
 
 
-def calculate_payment(principal, annual_interest, years, annual_payments):
-    """
+def calculate_payment(principal, annual_interest, annual_payments, years):
+    r"""
     Function to calculate an individual loan payment from the given params
+    Using the following formula (Written in LaTeX):
+    payment = principal \: \div \: \frac{1-(1+interest)^{-numPayments}}{interest}
     """
     num_payments = years * annual_payments
     interest = annual_interest / annual_payments
@@ -28,15 +32,15 @@ def prompt_user():
     the number of payments to be made each year, and the number of years the money
     is to be borrowed (in that order, one per line).
     """
-    print(f"Input Amount to be Borrowed:")
+    print(f"Input Amount to be Borrowed (In dollars):")
     principal = float(input())
-    print(f"Input Annual Interest Rate:")
+    print(f"Input Annual Interest Rate (Input as a number):")
     annual_interest = float(input())
-    print(f"Input Number of Years for loan to be paid:")
-    years = float(input())
     print(f"Input Number of Payments per year:")
     annual_payments = float(input())
-    return principal, annual_interest, years, annual_payments
+    print(f"Input Number of Years for loan to be paid:")
+    years = float(input())
+    return principal, annual_interest, annual_payments, years
 
 
 if __name__ == "__main__":
--- Assignment_1/loan_unittest.py
+++ Assignment_1/loan_unittest.py
@@ -3,8 +3,19 @@ import loan
 
 class TestCase(unittest.TestCase):
     def test_formula(self):
-        payment = loan.calculate_payment(10000, 0.12, 4, 12)
-        self.assertEqual(263.34, round(payment, 2))
+        # Format
+        # ((Principal, Interest, Annual Payments, Years), Expected Answer)
+        tests = (
+            ((10000, 0.12, 12, 4), 263.34),
+            ((680000, 0.0575, 12, 30), 3968.30),
+            ((275000000, 0.05, 4, 5), 13931182.65),
+            ((17000, 2.01, 52, 1), 13931182.65),
+        )
+        for test in tests:
+            print(f"Testing Args:{test[0]}, expecting answer {test[1]}")
+            payment = loan.calculate_payment(*test[0])
+            print(f"Got {payment:0.2f}")
+            self.assertEqual(test[1], round(payment, 2))
 
     def test_input(self):
         pass
