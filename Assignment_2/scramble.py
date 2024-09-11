605f09689db60e812f9040cf65448f815da046a5
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-09-11 12:22:58 -0700

    Update scramble.py

--- Assignment_2/scramble.py
+++ Assignment_2/scramble.py
@@ -17,7 +17,7 @@ import sys
 
 def main():
     print("Enter text, EOF to quit:")
-    sys.stdout.write(scramble_words(read_stdin()))
+    sys.stdout.writelines(scramble_words(read_stdin()))
 
 def read_stdin():
     user_input = ''
