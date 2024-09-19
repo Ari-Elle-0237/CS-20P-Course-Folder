6bc7efc5bb022d44a2fcdca1484cd6dc292f9432
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-09-19 14:32:03 -0700

    Assignment 3 initial commit

---
+++ Assignment_3/binary scratch.py
@@ -0,0 +1,28 @@
+# bits_a = 0b10110101
+# bits_b = 0b10111101
+bits_a = 0b10110101
+bits_b = 0b10110111
+
+xor_ab = bits_b ^ bits_a
+print(f"{xor_ab:08b}")
+# print(bin(bits_b ^ bits_a))
+# print(xor_ab.bit_count())
+print(bin(xor_ab).count("1"))
+print(xor_ab.bit_length())
+print(xor_ab.bit_count())
+
+def compare_bits(bits_a, bits_b):
+    # XOR A and B together, then count the number of ones in the resulting binary number to determine
+    # the number of differences between
+    # If your script is running earlier than python 3.10 then it should be bin(bits_a).count("1")
+    bit_count = (bits_a ^ bits_b).bit_count()
+    print(f"{bit_count=}")
+    if bit_count <= 7:
+        return bit_count
+    return -1
+
+print(compare_bits(bits_a,bits_b))
+
+
+if __name__ == "__main__":
+    pass
