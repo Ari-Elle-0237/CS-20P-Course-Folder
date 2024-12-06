63399db9ac8b5a3efb06b0b9e71cbd945744677d
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-12-06 03:23:18 -0800

    Added buggy remove at index

--- Assignment_6/20p-6/linked_list.py
+++ Assignment_6/20p-6/linked_list.py
@@ -8,9 +8,14 @@ Repository at: https://github.com/Ari-Elle-0237/CS-20P-Course-Folder.git
 Due: Dec 5th 2024
 Exit Code _:
 """
+import re
 
 class linked_list: # PEP8 violation req'd by assignment spec
     class Node:
+        """
+        Nodes are simple objects with two attributes, a 1 character string that they store (data),
+        and a link to the next node in the chain. Most of the code here is input sanitization
+        """
         def __init__(self, data=None, link=None):
             self.link = link
             self.data = data
@@ -30,10 +35,10 @@ class linked_list: # PEP8 violation req'd by assignment spec
         def __str__(self):
             return f"(Node: {self.data} | -> {self.link})"
 
-    def __init__(self, initial:""):
+    def __init__(self, initial:str=""):
         self.length = 0
         self.first = None
-        for i in reversed(initial):
+        for i in reversed(initial): # Reverse to preserve order correctly
             self.insert(i)
 
     def insert(self, item: str):
@@ -89,32 +94,27 @@ class linked_list: # PEP8 violation req'd by assignment spec
         self.length += len(chain_to_insert)
 
 
-    # def remove_at_index(self, index, span):
-    #     # Get the previous and next items in the list (if they exist) and save them
-    #     if index - 1 >= 0:
-    #         prev = self[index - 1]
-    #     if index + span <= len(self):
-    #         nxt = self[index + span]
-    #     else:
-    #         nxt = self[-1]
-    #
-    #     # Then update the links (if possible)
-    #     try:
-    #         prev.link = chain_to_insert[0]
-    #     except UnboundLocalError:
-    #         # If we're at the beginning of the list, prev won't exist, so instead we update self.first
-    #         self.first = chain_to_insert[0]
-    #     # Try/Except is not necessary for nxt since we can just assign None to it.
-    #     chain_to_insert[-1].link = nxt
-    #
-    #     # Lastly, update length
-    #     self.length += len(chain_to_insert)
-
-
+    def remove_at_index(self, index, count):
+        """
+        Deletes nodes from the list by breaking their links
+        :param index: index to begin from (inclusive)
+        :param count: number of nodes to be deleted
+        :return: None
+        """
+        if index + count + 1 < self.length:
+            self[index].link = self[index + count + 1]
+        elif index + count + 1 == self.length:
+            self[index].link = None
+        else:
+            raise IndexError("Index out of range")
+        self.length -= count
 
     def splice(self, find, replace):
+        # Sanitize Inputs
         if len(find) == 0:
             return
+
+
         for i in self:
             if i.data == find:
                 i.data = replace
@@ -145,9 +145,11 @@ def main():
     test = linked_list("2345")
     test.insert("1")
     test.append("6")
-    print(test[-1].data)
+    # print(test[-1].data)
     test.insert_at_index("ABC", 6)
     # print(test[2].data)
+    print(test)
+    test.remove_at_index(0, 3)
     print(test)
 
 if __name__ == "__main__":
